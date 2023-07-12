85132a0188f09f468b68995d0ce3fe506653b863
Marco Lima <marckdx@outlook.com>
2023-07-12 18:15:39 -0300

    adding project to the repo

---
+++ src/Proxfield.Parallel/DegreeOfParallelization.cs
@@ -0,0 +1,9 @@
+﻿namespace Proxfield.Parallel
+{
+    public static class DegreeOfParallelization
+    {
+        public const int Low = 1;
+        public const int Medium = 10;
+        public const int High = 100;
+    }
+}
---
+++ src/Proxfield.Parallel/Parallel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Proxfield.Parallel
+{
+    public class Parallel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="degreeOfParallelization"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public static Task ParallelForEachAsync<T>(
+            IEnumerable<T> source,
+            Func<T, Task> body,
+            int degreeOfParallelization = DegreeOfParallelization.Medium
+             )
+        {
+            async Task awaitPartition(IEnumerator<T> partition)
+            {
+                using (partition)
+                {
+                    while (partition.MoveNext())
+                    {
+                        await body(partition.Current);
+                    }
+                }
+            }
+
+            return Task.WhenAll(
+                Partitioner
+                .Create(source)
+                .GetPartitions(degreeOfParallelization)
+                .AsParallel()
+                .Select(awaitPartition));
+        }
+    }
+}
